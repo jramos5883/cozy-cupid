ab73f45a3eb6c538b126083140b9a4d090cbbdf8
jramos5883 <jramos5883@gmail.com>
2023-09-01 21:28:02 -0700

    checking on testi

--- cozy-cupid-website/app/testimonials/page.jsx
+++ cozy-cupid-website/app/testimonials/page.jsx
@@ -4,6 +4,7 @@ const bVP = Be_Vietnam_Pro({ weight: "700", subsets: ["latin"] });
 
 export default function Testimonials() {
   return (
+    // Need to check on Testimonials view on tablets
     <div className="flex flex-col justify-center items-center mx-auto bg-blue-200 bg-opacity-90 pt-20 pb-20 space-y-4 px-4">
       <span
         className={`${bVP.className} text-3xl text-center text-sky-700 pb-4`}
